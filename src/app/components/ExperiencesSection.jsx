d58ec9f971e0a95577c3e466db9ea26752be95e3
IndiartoAji13 <indiartoaji13@gmail.com>
2024-02-02 16:04:21 +0700

    Update ExperiencesSection.jsx

--- src/app/components/ExperiencesSection.jsx
+++ src/app/components/ExperiencesSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { BriefcaseIcon, ArrowUpRightIcon } from "@heroicons/react/24/solid";
+import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
 import { experienceList } from "../utils/experiencesList";
 
 const ExperiencesSection = () => {
