e9dbb235c7f14dbf477a5e39623d7cc897009cf5
IndiartoAji13 <indiartoaji13@gmail.com>
2024-02-02 14:58:48 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,11 @@
-import Image from "next/image";
-import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
-import AboutSection from "./components/AboutSection";
-import ProjectsSections1 from "./components/ProjectsSections1";
-import Footer from "./components/Footer";
-import AchivementsSection from "./components/AchivementsSection";
+import HeroSection from "./components/HeroSection";
 import ExperiencesSection from "./components/ExperiencesSection";
-import { JetBrains_Mono } from "next/font/google";
 import SkillsSection from "./components/SkillsSection";
 import ProjecstSections from "./components/ProjectsSections";
 import EducationsSection from "./components/EducationsSection";
+import Footer from "./components/Footer";
+import { JetBrains_Mono } from "next/font/google";
 
 const jetBrains = JetBrains_Mono({
   subsets: ["latin"],
@@ -22,15 +18,11 @@ const jetBrains = JetBrains_Mono({
 export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col ${jetBrains.className}`} data-theme="emerald">
-      {/* // <main className={`flex min-h-screen flex-col ${jetBrains.className}`} data-theme="dracula"> */}
       <Navbar />
       <div className="container mt-20 mx-auto py-4 px-8">
         <HeroSection />
         <ExperiencesSection />
         <SkillsSection />
-        {/* <AchivementsSection /> */}
-        {/* <AboutSection /> */}
-        {/* <ProjectsSections1 /> */}
         <ProjecstSections />
         <EducationsSection />
       </div>
