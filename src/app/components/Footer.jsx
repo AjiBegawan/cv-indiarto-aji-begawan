791520df1c20ae2a8a609a940c483c564c334b6a
IndiartoAji13 <indiartoaji13@gmail.com>
2024-02-02 16:03:43 +0700

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,12 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer  text-white bg-[#64A9A5]">
-      <div className="container p-6 flex justify-center">
-        <p className="text-white ">
-          Copyright ©2024 Indiarto Aji Begawan. All right reserved.
-        </p>
-      </div>
+    <footer className="footer bg-[#64A9A5] p-5 flex justify-center">
+      <p className="text-white">
+        Copyright ©2024 Indiarto Aji Begawan. All right reserved.
+      </p>
     </footer>
   );
 };
