b1f60751da57d1facc5ddde2cde079f007daaef1
IndiartoAji13 <indiartoaji13@gmail.com>
2024-02-27 10:38:03 +0700

    Update certificatesList.js

--- src/app/utils/certificatesList.js
+++ src/app/utils/certificatesList.js
@@ -10,93 +10,110 @@ export const certificateList = [
     org: "Google",
     name: "Analitik Data Google",
     icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg"
-  }, {
+  },
+  {
     id: 3,
     org: "Microsoft",
     name: "Microsoft Certified Azure Data Fundamentals",
     icon: "/images/icons/microsoft.svg"
-  }, {
+  },
+  {
     id: 4,
     org: "Microsoft",
     name: "Microsoft Certified Power Platform Fundamentals",
     icon: "/images/icons/microsoft.svg"
-  }, {
+  },
+  {
     id: 5,
     org: "Microsoft",
     name: "Microsoft Certified Security, Compliance, and Identity Fundamentals",
     icon: "/images/icons/microsoft.svg"
-  }, {
+  },
+  {
     id: 6,
     org: "Microsoft",
     name: "Microsoft Certified Azure AI Fundamentals",
     icon: "/images/icons/microsoft.svg"
-  }, {
+  },
+  {
+    id: 6,
+    org: "Binar",
+    name: "Data Science",
+    icon: "/images/icons/binar-academy-icon.jpeg"
+  },
+  {
     id: 7,
     org: "Dicoding",
     name: "Memulai Pemrograman Dengan Python",
     icon: "/images/icons/dicoding.svg"
-  }
-  , {
+  },
+  {
     id: 8,
     org: "Dicoding",
     name: "Belajar Machine Learning untuk Pemula",
     icon: "/images/icons/dicoding.svg"
-  }
-  , {
+  },
+  {
     id: 9,
     org: "Dicoding",
     name: "Belajar Pengembangan Machine Learning",
     icon: "/images/icons/dicoding.svg"
-  }
-  , {
+  },
+  {
     id: 10,
     org: "Dicoding",
     name: "Belajar Dasar Visualisasi Data",
     icon: "/images/icons/dicoding.svg"
-  }
-  , {
+  },
+  {
     id: 11,
     org: "Dicoding",
     name: "Belajar Dasar Pemrograman JavaScript",
     icon: "/images/icons/dicoding.svg"
-  }
-  , {
+  },
+  {
     id: 12,
     org: "Dicoding",
     name: "Belajar Dasar Pemrograman Web",
     icon: "/images/icons/dicoding.svg"
-  }
-  , {
+  },
+  {
     id: 13,
     org: "Dicoding",
     name: "Belajar Membuat Front-End Web untuk Pemula",
     icon: "/images/icons/dicoding.svg"
-  }, {
+  },
+  {
     id: 14,
     org: "Dicoding",
     name: "Belajar Membuat Aplikasi Web dengan React",
     icon: "/images/icons/dicoding.svg"
-  }, {
+  },
+  {
     id: 15,
     org: "Dicoding",
     name: "Belajar Membuat Aplikasi Back-End untuk Pemula",
     icon: "/images/icons/dicoding.svg"
-  }, {
+  },
+  {
     id: 16,
     org: "Dicoding",
     name: "Belajar Fundamental Aplikasi Back-End",
     icon: "/images/icons/dicoding.svg"
-  }, {
+  },
+  {
     id: 17,
     org: "Dicoding",
     name: "Cloud Practitioner Essentials (Belajar Dasar AWS Cloud)",
     icon: "/images/icons/dicoding.svg"
-  }, {
+  },
+  {
     id: 18,
     org: "Dicoding",
     name: "Belajar Dasar-Dasar DevOps",
     icon: "/images/icons/dicoding.svg"
-  }, {
+  },
+  {
     id: 19,
     org: "Dicoding",
     name: "Architecting on AWS (Membangun Arsitektur Cloud di AWS)",
